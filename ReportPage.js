65c4c71e6a9369b07e672f54ee07e8ff48762198
Abel Bancu <aeb56@kent.ac.uk>
2023-11-16 15:10:26 +0000

    Add new file

---
+++ ReportPage.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import './ReportPage.css';
+
+const ReportPage = () => {
+  const [item, setItem] = useState('');
+  const [description, setDescription] = useState('');
+  const [location, setLocation] = useState('');
+  const [quantity, setQuantity] = useState('');
+
+  const handleSubmit = () => {
+    // Handle the form submission, you can send the data to HR or perform any necessary actions
+    console.log('Submitted:', { item, description, location });
+    // You might want to reset the form after submission
+    setItem('');
+    setDescription('');
+    setLocation('');
+  };
+
+  return (
+    <div className="report-page">
+      <h1 className="report-title">Report Damaged or Broken Item</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="item">Item Name:</label>
+          <input
+            type="text"
+            id="item"
+            value={item}
+            onChange={(e) => setItem(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="Quantity">Quantity:</label>
+          <input
+          type="number"
+            id="quantity"
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+            required
+            min="0"
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="description">Description of Damage:</label>
+          <textarea
+            id="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="location">Location of Damage:</label>
+          <input
+            type="text"
+            id="location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            required
+          />
+        </div>
+
+        <button type="button" onClick={handleSubmit}>
+          Submit Report
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ReportPage;
