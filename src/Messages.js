dfe66063a773dd78882df0eb47ba161bfb8f5acb
Verone Mihindukulasuriya Thiserage <vsm2@kent.ac.uk>
2024-01-29 13:37:20 +0000

    backend applied code still not functional work in progress

--- src/Messages.js
+++ src/Messages.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'; //imports this file from the directory which is providing the CSS
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
 
 // The HTML of the Header section is saved under the below function which can be recalled so it can be rendered
@@ -27,22 +28,37 @@ function Option() {
       <Link to="/pages/view-inventory">View Inventory</Link>
       <Link to="/pages/order">Order Items</Link>
       <Link to="/pages/report">Report faulty/missing item</Link>
-      <Link to="/pages/mailbox">Mailbox</Link>
+      <Link to="/pages/mailbox" className="selected_tab" >Mailbox</Link>
     </div>
   );
 }
 
 function Messages() {
+  const [option, optionStatus] = useState(null);
+
+  const optionClick = (button) => {
+    optionStatus(button);
+  };
+
+  const contacts = ['Restaurant London Holborn', 'Verone'];
+
   return (
     <>
     <div className="requests">
         <div>Messages</div>
-        <a>Restaurant London Holborn<br></br>19/11/2024 4:38pm</a>
+        {contacts.map((button, index) => (
+          <button key={index} onClick={() => optionClick(button)}>
+            {button}
+          </button>
+        ))}
     </div>
 
     <div className="description">
-        <div>Restaurant London HolbornRestaurant London Holborn</div>
-        <div>Restaurant London HolbornRestaurant London Holborn</div>
+        {optionStatus && (
+        <div>
+          //db code to be added 
+        </div>
+      )}
     </div>
 
     <div className="textcontainer">
@@ -69,7 +85,7 @@ function App() {
         <Header />
         <Option />
         <Route path="/pages/view-inventory" component={ViewInventory} />
-        <Route path="/pages/order" component={catalogue} />
+        <Route path="/pages/catalogue" component={catalogue} />
         <Route path="/pages/report" component={Report} />
         <Route path="/pages/mailbox" component={Mailbox} />
       </div>
