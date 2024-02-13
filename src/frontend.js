6249c8ba5345a77f882ee01888defe86e95a6fac
Verone Mihindukulasuriya Thiserage <vsm2@kent.ac.uk>
2024-02-13 14:17:50 +0000

    Access Levels have been separated

--- src/frontend.js
+++ src/frontend.js
@@ -26,28 +26,65 @@ const Header = () => {
 
 // The HTML of the side options bar section is saved under the below function which can be recalled so it can be rendered
 function Option() {
+  
+  const access = 'headoffice'; // Replacing this with db access level
+
   return (
     <div id="leftPane" className="options">
-      <NavLink to="/" className={Catalogue}>
-        Order items
-      </NavLink>
-      <NavLink to="/create" className={Create}>
-        Create Restaurant
-      </NavLink>
-      <NavLink to="/createam" className={CreateAM}>
-        Create Area Manager
-      </NavLink>
-      <NavLink  to="/" className={Catalogue}>
-        Report faulty/ missing item
-      </NavLink>
-      <NavLink to="/modify" className={Modify}>
-        Modify User
-      </NavLink>
-      <NavLink to="/message" className={Message}>
-        Mailbox
-      </NavLink>
+      {access === 'restaurant' && (
+        <>
+          <NavLink to="/" className={Catalogue}>
+            View Inventory
+          </NavLink>
+          <NavLink to="/" className={Catalogue}>
+            Order items
+          </NavLink>
+          <NavLink to="/" className={Catalogue}>
+            Report faulty/ missing item
+          </NavLink>
+          <NavLink to="/message" className={Message}>
+            Mailbox
+          </NavLink>
+        </>
+      )}
+      {access === 'am' && (
+        <>
+          <NavLink to="/" className={Catalogue}>
+            POS request Orders
+          </NavLink>
+          <NavLink to="/report" className={Report}>
+            Report page
+          </NavLink>
+          <NavLink to="/report" className={Report}>
+            Budget tracker
+          </NavLink>
+          <NavLink to="/message" className={Message}>
+            Mailbox
+          </NavLink>
+        </>
+      )}
+      {access === 'headoffice' && (
+        <>
+          <NavLink to="/report" className={Report}>
+            Report page
+          </NavLink>
+          <NavLink to="/create" className={Create}>
+            Create Restaurant
+          </NavLink>
+          <NavLink to="/createam" className={CreateAM}>
+            Create Area Manager
+          </NavLink>
+          <NavLink to="/modify" className={Modify}>
+            Modify User
+          </NavLink>
+          <NavLink to="/message" className={Message}>
+            Mailbox
+          </NavLink>
+        </>
+      )}
     </div>
   );
+  
 }
 
 
