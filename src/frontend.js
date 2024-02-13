bd6e4120dc7fea503046ee4a8631d97fa0d05e48
Verone Mihindukulasuriya Thiserage <vsm2@kent.ac.uk>
2024-02-13 14:54:52 +0000

    Approval tab added

--- src/frontend.js
+++ src/frontend.js
@@ -9,6 +9,8 @@ import CreateAM from './pages/createAM';
 import Message from './pages/message';
 import Modify from './pages/modify';
 import Report from './pages/report';
+import Approval from './pages/approval';
+
 
 // The HTML of the Header section is saved under the below function which can be recalled so it can be rendered
 const Header = () => {
@@ -26,9 +28,9 @@ const Header = () => {
 }
 
 // The HTML of the side options bar section is saved under the below function which can be recalled so it can be rendered
-function Option() {
-  
-  const access = 'headoffice'; // Replacing this with db access level
+const Option = () => {
+  // Assuming `access` is a variable accessible in this scope
+  const access = 'am'; // Replace this with your actual logic
 
   return (
     <div id="leftPane" className="options">
@@ -50,7 +52,7 @@ function Option() {
       )}
       {access === 'am' && (
         <>
-          <NavLink to="/" className={Catalogue}>
+          <NavLink to="/approval" className={Approval}>
             POS request Orders
           </NavLink>
           <NavLink to="/report" className={Report}>
@@ -85,7 +87,6 @@ function Option() {
       )}
     </div>
   );
-  
 }
 
 
