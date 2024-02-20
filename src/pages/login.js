2a0f0900ecf5fdf2f3c487b7021740312a42fbda
Samuel Ward <sw714@kent.ac.uk>
2024-02-20 15:19:33 +0000

    changed css of loginpage

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,41 +1,44 @@
 import React, { useState } from 'react';
+import logo from '../assets/logo.png';
+import './index.css';
 
 const LoginPage = ({ onLogin }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-  const submitButton = (event) => {
-    event.preventDefault();
-    // login code here
-    onLogin(username, password);
-  };
+    const submitButton = (event) => {
+        event.preventDefault();
+        // login code here
+        onLogin(username, password);
+    };
 
-  return (
-    <div className='login-container'>
-      <h2>Login</h2>
-      <form onSubmit={submitButton} className='login-form'>
-        <div className='form-group'>
-          <label htmlFor='username'>Username:</label>
-          <input
-            type='text'
-            id='username'
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+    return (
+        <div className='login-container login-page-background'>
+            <img src = {logo} alt = "Logo" className="login-logo"/>
+            <h2 className="logintext">LOGIN</h2>
+            <form onSubmit={submitButton} className='login-form'>
+                <div className='form-group'>
+                    <label htmlFor='username'>USERNAME</label>
+                    <input
+                        type='text'
+                        id='username'
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='password'>PASSWORD</label>
+                    <input
+                        type='password'
+                        id='password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <button type='submit' className='login-button'>Login</button>
+            </form>
         </div>
-        <div className='form-group'>
-          <label htmlFor='password'>Password:</label>
-          <input
-            type='password'
-            id='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type='submit' className='login-button'>Login</button>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default LoginPage;
