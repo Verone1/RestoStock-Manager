43ae7ccde97933ba7dcc02a34ebe2629973dd1cd
Samuel Ward <sw714@kent.ac.uk>
2024-02-20 11:57:44 +0000

    added login and logout code

--- src/frontend.js
+++ src/frontend.js
@@ -11,16 +11,15 @@ import Modify from './pages/modify';
 import Report from './pages/report';
 import Approval from './pages/approval';
 
-
 // The HTML of the Header section is saved under the below function which can be recalled so it can be rendered
-const Header = () => {
+const Header = ({ onLogout }) => {
   return (
     <header className="header">
       <img src={logo}></img>
       <div className= "icon">
         <button className="icon-button">VT</button>
         <div className="icon-list">
-          <div href="#">logout</div>
+          <button onClick={onLogout}>Logout</button>
         </div>
       </div>
     </header>
@@ -30,7 +29,7 @@ const Header = () => {
 // The HTML of the side options bar section is saved under the below function which can be recalled so it can be rendered
 const Option = () => {
   // Assuming `access` is a variable accessible in this scope
-  const access = 'am'; // Replace this with your actual logic
+  const access = 'headoffice'; // Replace this with your actual logic
 
   return (
     <div id="leftPane" className="options">
