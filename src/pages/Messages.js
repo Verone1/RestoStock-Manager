a389c513f1452c404ad881bb6aa55d5c6ef63eeb
Verone Mihindukulasuriya Thiserage <vsm2@kent.ac.uk>
2024-03-03 15:51:44 +0000

    removed unnecessary code

--- src/pages/Messages.js
+++ src/pages/Messages.js
@@ -1,36 +1,6 @@
 import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+
 import './index.css'; //imports this file from the directory which is providing the CSS
-
-
-
-// The HTML of the Header section is saved under the below function which can be recalled so it can be rendered
-function Header() {
-  return (
-    <header className="header">
-      <a> Home </a>
-      <p>Hospitality Stock System </p>
-      <div className= "icon">
-        <button className="icon-button">VT</button>
-        <div className="icon-list">
-          <div href="#">Sign out</div>
-        </div>
-      </div>
-    </header>
-  );
-}
-
-// The HTML of the side options bar section is saved under the below function which can be recalled so it can be rendered
-function Option() {
-  return (
-    <div id="leftPane" className="options">
-      <a href="#">View Inventory</a>
-      <a href="order">Order Items </a>
-      <a href="#">Report faulty/ missing item</a>
-      <a href="#">Mailbox</a>
-    </div>
-  );
-}
 
 function Messages() {
   const [option, optionStatus] = useState(null);
@@ -74,26 +44,3 @@ function Messages() {
 
 export default Messages;
 
-
-
-// All functions saved under the APP function
-function App() {
-  return (
-    
-      <div>
-        <Header />
-        <Option />
-        <Messages />
-      </div>
-  );
-}
-
-// Below code renders all functions stored under App()
-ReactDOM.render(<App />, document.getElementById('root')); 
-
-
-
-
-
-// Below code renders all functions stored under App()
-ReactDOM.render(<App />, document.getElementById('root')); 
