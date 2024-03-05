1d09aa349842b1cddb6a3b7057d248d86fcdf604
Samuel Ward <sw714@kent.ac.uk>
2024-03-05 10:46:53 +0000

    verone changes

--- src/pages/createam.js
+++ src/pages/createam.js
@@ -1,45 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import '../index.css';
 
 
 const CreateAM = () => {
   return (
     <div className="creation">
-          <div className="description">
-            <div>
-              <a>*</a>
-              <label>Name:</label>
-              <input type="text" id="fields" placeholder="Enter Name" /><br />
-            </div>
-            <div>
-              <a>*</a>
-              <label>Phone Number:</label>
-              <input type="text" id="fields" placeholder="Enter Number" /><br />
-            </div>
-            <div>
-              <a>*</a>
-              <label>Budget:</label>
-              <input data-type="currency" id="fields" placeholder="Enter Budget" /><br />
-            </div>
-            <div>
-              <a>*</a>
-              <label>Restaurants:</label>
-              <div id="restaurants">
-                <input id="fields" type="checkbox"/>
-                <label for="fields"> Restaurant 1</label>
-                <input id="fields" type="checkbox"/>
-                <label for="fields"> Restaurant 2</label>
-                <input id="fields" type="checkbox"/>
-                <label for="fields"> Restaurant 1</label>
-                <input id="fields" type="checkbox"/>
-                <label for="fields"> Restaurant 2</label>
+          <div>
+            <a>*</a>
+            <label>Name:</label>
+            <input type="text" id="fields" placeholder="Enter Name" /><br />
+          </div>
+          <div>
+            <a>*</a>
+            <label>Phone Number:</label>
+            <input type="text" id="fields" placeholder="Enter Number" /><br />
+          </div>
+          <div>
+            <a>*</a>
+            <label>Budget:</label>
+            <input data-type="currency" id="fields" placeholder="Enter Budget" /><br />
+          </div>
+          <div>
+            <a>*</a>
+            <label>Restaurants:</label>
+            <div id="restaurants">
+              <div id="fields">
+              <input id="fields" type="checkbox"/>
+              <label for="fields"> Restaurant 1</label>
+              <input id="fields" type="checkbox"/>
+              <label for="fields"> Restaurant 2</label>
+              <input id="fields" type="checkbox"/>
+              <label for="fields"> Restaurant 1</label>
+              <input id="fields" type="checkbox"/>
+              <label for="fields"> Restaurant 2</label>
+              <input id="fields" type="checkbox"/>
+              <label for="fields"> Restaurant 2</label>
               </div>
             </div>
-            <div>
-              <input id="save" type="submit" value="Save" />
-              
-            </div>
+          </div>
+          <div>
+            <input id="save" type="submit" value="Save" />
           </div>
         </div>
   );
@@ -47,13 +47,3 @@ const CreateAM = () => {
 
 export default CreateAM;
 
-function App() {
-  return (
-
-      <div>
-        <CreateAM />
-      </div>
-  );
-}
-
-ReactDOM.render(<App />, document.getElementById('root')); 
