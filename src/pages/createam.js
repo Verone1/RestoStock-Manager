ca894457e48a60775302a5140e1b492a64f641c3
Samuel Ward <sw714@kent.ac.uk>
2024-03-05 19:26:20 +0000

    fixing create area manager page git issues

---
+++ src/pages/createam.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import '../index.css';
+
+
+const CreateAM = () => {
+  return (
+    <div className="areamanager-page-container">
+          <div>
+            <a>*</a>
+            <label>Name:</label>
+            <input type="text" id="area-manager-page-fields" placeholder="Enter Name" /><br />
+          </div>
+          <div>
+            <a>*</a>
+            <label>Phone Number:</label>
+            <input type="text" id="area-manager-page-fields" placeholder="Enter Number" /><br />
+          </div>
+          <div>
+            <a>*</a>
+            <label>Budget:</label>
+            <input data-type="currency" id="area-manager-page-fields" placeholder="Enter Budget" /><br />
+          </div>
+          <div>
+            <a>*</a>
+            <label>Restaurants:</label>
+            <div id="areamanager-page-restaurants-list">
+              <div id="area-manager-page-fields">
+              <input id="fields" type="checkbox"/>
+              <label for="fields"> Restaurant 1</label>
+              <input id="fields" type="checkbox"/>
+              <label for="fields"> Restaurant 2</label>
+              <input id="fields" type="checkbox"/>
+              <label for="fields"> Restaurant 1</label>
+              <input id="fields" type="checkbox"/>
+              <label for="fields"> Restaurant 2</label>
+              <input id="fields" type="checkbox"/>
+              <label for="fields"> Restaurant 2</label>
+              </div>
+            </div>
+          </div>
+          <div>
+            <input id="areamanager-page-save-button" type="submit" value="Save" />
+          </div>
+        </div>
+  );
+};
+
+export default CreateAM;
