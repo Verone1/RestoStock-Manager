841698388a70ff526956e887be43e2f1ac16c5e3
Samuel Ward <sw714@kent.ac.uk>
2024-03-05 19:36:11 +0000

    fixing and formatting create restaurant css

--- src/pages/create.js
+++ src/pages/create.js
@@ -4,26 +4,26 @@ import '../index.css';
 
 const Create = () => {
   return (
-    <div className="creation">
+    <div className="createres-page-container">
       <div>
         <a>*</a>
         <label>Name:</label>
-        <input type="text" id="fields" placeholder="Enter Name" /><br />
+        <input type="text" id="createres-page-fields" placeholder="Enter Name" /><br />
       </div>
       <div>
         <a>*</a>
         <label>Phone Number:</label>
-        <input type="text" id="fields" placeholder="Enter Number" /><br />
+        <input type="text" id="createres-page-fields" placeholder="Enter Number" /><br />
       </div>
       <div>
         <a>*</a>
         <label>Address:</label>
-        <input id="fields" placeholder="Enter Address" /><br />
+        <input id="createres-page-fields" placeholder="Enter Address" /><br />
       </div>
       <div>
         <a>*</a>
         <label>Area Manager:</label>
-        <select id="fields">
+        <select id="createres-page-fields">
           <option>AM example</option>
         </select>
       </div>
