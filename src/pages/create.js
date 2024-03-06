4aca344e3bc3c97a382833032e0aa0e7ef1a5890
Samuel Ward <sw714@kent.ac.uk>
2024-03-06 12:33:03 +0000

    reformatting CSS for create restaurant page to make more global variables

--- src/pages/create.js
+++ src/pages/create.js
@@ -12,22 +12,22 @@ const Create = () => {
       <div>
         <a>*</a>
         <label>Name:</label>
-        <input type="text" id="createres-page-fields" placeholder="Enter Name" /><br />
+        <input type="text" id="page-fields" placeholder="Enter Name" /><br />
       </div>
       <div>
         <a>*</a>
         <label>Phone Number:</label>
-        <input type="text" id="createres-page-fields" placeholder="Enter Number" /><br />
+        <input type="text" id="page-fields" placeholder="Enter Number" /><br />
       </div>
       <div>
         <a>*</a>
         <label>Address:</label>
-        <input id="createres-page-fields" placeholder="Enter Address" /><br />
+        <input id="page-fields" placeholder="Enter Address" /><br />
       </div>
       <div>
         <a>*</a>
         <label>Area Manager:</label>
-        <select id="createres-page-fields">
+        <select id="page-fields">
           <option>AM example</option>
         </select>
       </div>
